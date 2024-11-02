2f4767994d34279558852c4f1bbc751c7095d4e7
uanik <uanik7150@gmail.com>
2024-11-02 11:59:23 +0530

    day2

--- main.go
+++ main.go
@@ -3,35 +3,64 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
 )
 
-func main(){
+func main() {
 
-	l, err := net.Listen("tcp",":6379")
+	l, err := net.Listen("tcp", ":6379")
 	if err != nil {
 		fmt.Println(err)
-		return 
+		return
 	}
 
+	aof, err := NewAof("db.aof")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer aof.Close()
+
 	conn, err := l.Accept()
-	if err != nil{
+	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer conn.Close()
 
-	for{
+	for {
 		resp := NewResp(conn)
 		value, err := resp.Read()
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		_=value
+		if value.typ != "array" {
+			fmt.Println("Invalid req")
+			continue
+		}
+
+		if len(value.array) == 0 {
+			fmt.Println("expexted len is > than 0")
+			continue
+		}
+
+		command := strings.ToUpper(value.array[0].bulk)
+		args := value.array[1:]
 
 		writer := NewWriter(conn)
-		writer.Write(Value{typ: "string", str: "OK"})
 
+		handler, ok := Handlers[command]
+		if !ok {
+			fmt.Println("Invalid command: ", command)
+			writer.Write(Value{typ: "string", str: ""})
+			continue
+		}
+		if command == "SET" || command == "HSET" {
+			aof.write(value)
+		}
+		result := handler(args)
+		writer.Write(result)
 
 	}
 }
